7d10388644d80c443c710d6d033e370e344668b0
Dubesor22 <dubesor22@gmail.com>
2022-08-11 13:25:05 +0200

    image Picker react-Native

--- App.js
+++ App.js
@@ -1,10 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+// import uploadToAnonymousFilesAsync from "anonymous-files";
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openImagePickerAsync = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access media library was denied");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    // console.log(pickerResult);
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    //not working anymore
+    // if (Platform.OS === "web") {
+    //   const remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+    //   setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    // }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  const openShareDialogAsync = async () => {
+    const permissionResult = await Sharing.isAvailableAsync();
+    if (!permissionResult) {
+      alert("Share is not available in your device");
+      return;
+    }
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Selector de Imagenes</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          style={styles.image}
+          source={{
+            uri:
+              selectedImage !== null
+                ? selectedImage.localUri
+                : "https://random-memer.herokuapp.com/",
+          }}
+        />
+      </TouchableOpacity>
+      {selectedImage !== null ? (
+        <View>
+          <Button title="Compartir" onPress={openShareDialogAsync} />
+        </View>
+      ) : (
+        <></>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +77,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#292929",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 30,
+    margin: 20,
   },
 });
